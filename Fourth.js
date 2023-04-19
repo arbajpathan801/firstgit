b2084750fa840aa36d65d9dfcbc10c30fa6611af
ArbajPathan <arbajpatel0432>
2023-04-19 14:34:09 +0530

    This is Fourth file

---
+++ Fourth.js
@@ -0,0 +1,8 @@
+const li=document.getElementsByTagName("li");
+console.log (li)
+for (let i=0;i<li.length;i++)
+{
+    li[i].style.backgroundColor="#ccc"
+}
+const LI=document.getElementsByClassName("list-group-item");
+console.log (LI)
