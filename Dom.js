7b9c1e9c4951d5466b514512e286c0a25132bbcb
ArbajPathan <arbajpatel0432>
2023-04-26 01:58:12 +0530

    done with dom

--- Dom.js
+++ Dom.js
@@ -1,5 +1,30 @@
+const itemList=document.querySelector("#items");
+ console.log(itemList.parentElement);
+ console.log(itemList.lastElementChild.textContent="hello 4")
+ console.log (itemList.lastChild)
+console.log (itemList.firstElementChild)
+console.log (itemList.firstChild)
+console.log (itemList.nextSibling)
+console.log (itemList.nextElementSibling)
+console.log (itemList.previousSibling)
+console.log (itemList.previousElementSibling)
+const newDiv=document.createElement('div')
+newDiv.className="Hello";
+newDiv.id="HEllo1"
+newDiv.setAttribute('title','Hello Div')
+const newDivText=document.createTextNode('HEllo World')
+newDiv.appendChild(newDivText)
+console.log (newDiv)
+const container=document.querySelector("header .container")
+const h1=document.querySelector("header h1")
 
+newDiv.style.fonrSize="30px"
+container.insertBefore(newDiv,h1)
 
-const AddItem=document.getElementById("second-title");
-AddItem.style.color="green"
+const parentnode=document.getElementById('items')
+const newLi=document.createElement('li')
+const text=document.createTextNode("hEllo world")
+newLi.appendChild(text)
+console.log(newLi)
 
+parentnode.innerHTML=newLi+parentnode.innerHTML
