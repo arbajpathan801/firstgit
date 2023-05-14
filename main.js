b3a164b26ca10a59786c812707c331b46a3e1fd4
ArbajPathan <arbajpatel0432>
2023-05-14 17:21:25 +0530

    done with deletebtn

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const nameInput=document.querySelector("#name")
 const emailInput=document.querySelector("#email")
 const msg=document.querySelector(".msg")
 const userList=document.querySelector("#Users")
+const dltbtn=document.createElement('button')
+dltbtn.appendChild(document.createTextNode("Delete"))
 
 // add event listener
 myform.addEventListener('submit',onsubmit)
+dltbtn.addEventListener('click' , onclick)
 let x=1;
 
 function onsubmit(e){
@@ -17,15 +20,24 @@ function onsubmit(e){
     }
     else {
         const li=document.createElement("li");
-        li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`))
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`))
+        li.appendChild(dltbtn)
         userList.appendChild(li)
         // Reset name and email
         setTimeout(()=>  (nameInput.value='',
         emailInput.value=''),1000);
-        localStorage.removeItem('UserList')
        localStorage.setItem(x,`${nameInput.value}:${emailInput.value}`)
        x=x+1
       
     }
 }
+
+function onclick(e){
+   
+      if(confirm('Are You Sure?')){
+        var li = e.target.parentElement;
+        userList.removeChild(li);
+      
+    }
+  }
 
