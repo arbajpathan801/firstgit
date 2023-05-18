fd926cd33e72a2ef667354f50d3a287da0838fbf
ArbajPathan <arbajpatel0432>
2023-05-18 09:27:52 +0530

    done with edit btn

--- main.js
+++ main.js
@@ -1,43 +1,69 @@
-const myform=document.querySelector("#my-form")
-const nameInput=document.querySelector("#name")
-const emailInput=document.querySelector("#email")
-const msg=document.querySelector(".msg")
-const userList=document.querySelector("#Users")
-const dltbtn=document.createElement('button')
-dltbtn.appendChild(document.createTextNode("Delete"))
+const myform = document.querySelector("#my-form")
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const msg = document.querySelector(".msg")
+const List = document.querySelector("#Users")
+
 
 // add event listener
-myform.addEventListener('submit',onsubmit)
-dltbtn.addEventListener('click' , onclick)
-let x=1;
+myform.addEventListener('submit', onsubmit)
 
-function onsubmit(e){
+
+function onsubmit(e) {
     e.preventDefault();
-    if (nameInput.value==='' || emailInput.value===''){
+    if (nameInput.value === '' || emailInput.value === '') {
         msg.classList.add('error');
-        msg.innerHTML='Please inter all fields'
-        setTimeout(()=> msg.remove(),3000)
+        msg.innerHTML = 'Please inter all fields'
+        setTimeout(() => msg.remove(), 3000)
+
     }
     else {
-        const li=document.createElement("li");
+
+        const dltbtn = document.createElement('button')
+        dltbtn.appendChild(document.createTextNode("Delete"))
+        dltbtn.value = "Delete"
+         const editbtn=document.createElement('button')
+         editbtn.appendChild(document.createTextNode("Edit"))
+        const li = document.createElement("li");
+
         li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`))
         li.appendChild(dltbtn)
-        userList.appendChild(li)
-        // Reset name and email
-        setTimeout(()=>  (nameInput.value='',
-        emailInput.value=''),1000);
-       localStorage.setItem(x,`${nameInput.value}:${emailInput.value}`)
-       x=x+1
-      
-    }
-}
+         li.appendChild(editbtn)
+        List.appendChild(li)
+         editbtn.addEventListener('click' , onpress)
+        dltbtn.addEventListener('click', onclick)
+        let obj = {
+            name: nameInput.value,
+            email: emailInput.value
+        };
+        let myobj = JSON.stringify(obj)
+        localStorage.setItem(nameInput.value, myobj);
+        const dlt=nameInput.value;
+        const back=emailInput.value;
+        setTimeout(() => (nameInput.value='',
+        emailInput.value=''),300 )
+        
+        function onclick(e) {
 
-function onclick(e){
    
-      if(confirm('Are You Sure?')){
-        var li = e.target.parentElement;
-        userList.removeChild(li);
-      
-    }
-  }
+            var li = e.target.parentElement;
+            List.removeChild(li);
+            
+            localStorage.removeItem(dlt)
+        }
+        function onpress(e) {
+
+   
+            var li = e.target.parentElement;
+            List.removeChild(li);
+            emailInput.value=back
+            nameInput.value=dlt
+            localStorage.removeItem(dlt)
+        }
+
+        }
+   
+}
+ 
+
 
